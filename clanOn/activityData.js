034d0f9da0e44655a48ed16f1cad22fa03dd09ff
Ian Littke <ian.littke@gmail.com>
2020-08-22 00:33:40 -0700

    add location data to members list

---
+++ clanOn/activityData.js
@@ -0,0 +1,121 @@
+const {getBungieRequest} = require('./fetch.js');
+const activityManifest = require('./manifestActivity.json');
+const modeManifest = require('./manifestMode.json');
+const destinationManifest = require('./manifestDestination.json');
+
+async function getActivityData(membershipType, membershipId, bungieToken){
+    const request = await getBungieRequest(`Destiny2/${membershipType}/Profile/${membershipId}`, bungieToken, {components: '200,204,1000'}); 
+  if (request.ErrorCode != 1){
+    return;
+  }
+  const profile = request.Response;
+  if (profile === undefined){
+    return;
+  }
+
+
+  return Object.keys(profile.characterActivities.data).map(character => {
+    const lastActivity = profile.characterActivities.data[character];
+    const definitionActivity = lastActivity.currentActivityHash && activityManifest[lastActivity.currentActivityHash];
+    const definitionActivityMode = lastActivity.currentActivityModeHash && modeManifest[lastActivity.currentActivityModeHash];
+    const definitionDestination = destinationManifest[definitionActivity.destinationHash];
+    const definitionActivityPlaylist = lastActivity.currentPlaylistActivityHash && activityManifest[lastActivity.currentPlaylistActivityHash]
+    let lastActivityString = false;
+    if (definitionActivity && !definitionActivity.redacted) {
+        if (definitionActivity.placeHash === 2961497387) {
+            // Orbit
+    
+            lastActivityString = "In Orbit";
+          } 
+
+
+           else if (definitionActivityMode && definitionActivityMode.hash === 3497767639) {
+          // Explore
+  
+            if (
+                definitionDestination.displayProperties.name &&
+                definitionActivity.displayProperties.name !== definitionDestination.displayProperties.name &&
+                // because fuck
+                definitionActivity.hash !== 4166562681 &&
+                definitionActivity.hash !== 4159221189 &&
+                definitionActivity.hash !== 3966792859
+            ) {
+                lastActivityString = `${definitionDestination.displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+            } else if (definitionDestination) {
+                lastActivityString = `${definitionDestination.displayProperties.name}: ${definitionActivityMode.displayProperties.name}`;
+            } else {
+                lastActivityString = `${definitionActivityMode.displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+            }
+        } else if (definitionActivity && definitionActivity.activityTypeHash === 400075666) {
+          // Menagerie
+  
+          lastActivityString = `${definitionActivity.selectionScreenDisplayProperties.name ? definitionActivity.selectionScreenDisplayProperties.name : definitionActivity.displayProperties && definitionActivity.displayProperties.name}`;
+        } else if (lastActivity.currentActivityModeHash === 547513715) {
+          // Nightfalls
+  
+          lastActivityString = definitionActivity.displayProperties.name;
+        } else if (definitionActivity && definitionActivity.activityTypeHash === 838603889) {
+          // Forge Ignition
+  
+          lastActivityString = `${definitionActivity.displayProperties.name}: ${definitionActivityPlaylist.displayProperties.name}`;
+        } else if (definitionActivity && definitionActivity.activityTypeHash === 3005692706 && definitionActivity.placeHash === 4148998934) {
+          // The Reckoning
+  
+          lastActivityString = `${definitionActivity.displayProperties.name}`;
+        } else if (lastActivity.currentActivityModeHash === 1848252830){
+            // Gambit
+
+            lastActivityString = `${definitionActivityMode.displayProperties.name}: ${definitionDestination.displayProperties.name}`;
+        }
+        
+        else if (lastActivity.currentActivityModeTypes.indexOf(5) > -1) {
+          // Crucible
+  
+          lastActivityString = `${modeManifest[1164760504].displayProperties.name}: ${definitionActivityPlaylist.displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+        } else if ([135537449, 740891329].includes(lastActivity.currentPlaylistActivityHash)) {
+          // Survival, Survival: Freelances
+  
+          lastActivityString = `${definitionActivityPlaylist.displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+        } else if (definitionActivity.activityTypeHash === 332181804) {
+          // Nightmare Hunts
+  
+          lastActivityString = definitionActivity.displayProperties.name;
+        } else if (definitionActivityPlaylist.hash === 2032534090) {
+          // Convert Story: The Shattered Throne -> Dungeon: The Shattered Throne
+  
+          lastActivityString = `${activityManifest[608898761].displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+        } else if (definitionActivityPlaylist.hash === 4148187374) {
+          // Convert Raid: Prophecy -> Dungeon: Prophecy
+  
+          lastActivityString = `${activityManifest[608898761].displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+        } else if (definitionActivityMode) {
+          // Default
+  
+          lastActivityString = `${definitionActivityMode.displayProperties.name}: ${definitionActivity.displayProperties.name}`;
+        } else {
+          lastActivityString = definitionActivity.displayProperties.name;
+        }
+      } else if (definitionActivity && definitionActivity.redacted) {
+        lastActivityString = t('Classified');
+      } else {
+        lastActivityString = false;
+      }
+
+    const lastMode = (definitionActivityMode && definitionActivityMode.parentHashes && definitionActivityMode.parentHashes.map((hash) => modeManifest[hash])) || [];
+    const joinability = profile.profileTransitoryData.data.joinability;
+    return {
+      'name': lastActivityString,
+      'mode': lastMode,
+      'definitionActivityMode': definitionActivityMode,
+      'definitionActivityPlaylist': definitionActivityPlaylist,
+      'inMatchMaking': joinability.openSlots > 0 && joinability.closedReasons == 1,
+      'matchmakingProperties': definitionActivityPlaylist && definitionActivityPlaylist.matchmaking || definitionActivity.matchmaking
+    }
+  }).sort((a, b) => {
+    if (a.lastPlayed > b.lastPlayed)
+      return -1;
+    return 1;
+  })[0];
+}
+
+module.exports = { getActivityData };
--- clanOn/bot.js
+++ clanOn/bot.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.io');
 require('dotenv').config();
 const {getBungieRequest} = require('./fetch.js');
-const activityManifest = require('./manifestActivity.json');
-const modeManifest = require('./manifestMode.json');
-const destinationManifest = require('./manifestDestination.json');
+const {getActivityData} = require('./activityData.js');
+
 const prefix = '!';
 const bungieToken = process.env.TOKEN;
 const groupId = process.env.GROUPID;
@@ -64,21 +63,21 @@ async function getOnlineMembers(){
     }));
     
     //console.log(allOnlineMembers);
-
+//${member.activity.inMatchMaking ? '(MM)' : ''}
     var fields = [];
 
     for(var platform in PlatformDict){
       var onlineMembers = getUsersByPlatform(allOnlineMembers, PlatformDict[platform]);
-      if (onlineMembers.length > 0)
-        console.log(onlineMembers);
+      if (onlineMembers.length > 0) {
         fields.push(
           {
             'name': `${platform} (${onlineMembers.length})`,
             'value': onlineMembers.map(member => {
-              return `${member.name} ${member.activity.name}${member.isMatchMaking ? '(MM)' : ''}`
+              return `${member.name} > ${member.activity.name} `
             }).join(' \n '),
           }
         )
+      }
     }
 
     const logMessage = {
@@ -102,37 +101,7 @@ function getUsersByPlatform(membersList, platform){
 }
 
 async function getMemberInfo(membershipType, membershipId){
-  const request = await getBungieRequest(`Destiny2/${membershipType}/Profile/${membershipId}`, bungieToken, {components: '200,204,1000'}); 
-  if (request.ErrorCode != 1){
-    return;
-  }
-  const profile = request.Response;
-  if (profile === undefined){
-    return;
-  }
-
-  // console.log(profile.characterActivities.data);
-  var foo = Object.keys(profile.characterActivities.data).map(character => {
-    var mode = profile.characterActivities.data[character].currentActivityModeHash;
-    var activityHash = profile.characterActivities.data[character].currentActivityHash;
-    var activity = activityManifest[activityHash];
-    var destinationHash = activity && activity.destinationHash;
-    var destination = destinationManifest[destinationHash];
-    return {
-      'id': character,
-      'lastPlayed': profile.characterActivities.data[character].dateActivityStarted,
-      'activityHash': activity,
-      'name': `${modeManifest[mode] && modeManifest[mode].displayProperties.name} ${activityManifest[activityHash] && activityManifest[activityHash].displayProperties.name}`,
-      'destination': destination && destination.displayProperties.name,
-      'inMatchMaking': profile.profileTransitoryData.data.joinability.closedReasons == 1
-    }
-  }).sort((a, b) => {
-    if (a.lastPlayed > b.lastPlayed)
-      return -1;
-    return 1;
-  });
-
-  return foo[0];
+  return await getActivityData(membershipType, membershipId, bungieToken);
 }
 
 //this is hardcoded from the bungieAPI
